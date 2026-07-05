196b95ce3df22aeb7b4c2ccc14ce45598b7eb911
jade1234-tech <occ.dadang.jade1234@gmail.com>
2026-07-05 15:23:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Active Link Indicator Highlight Strategy on Page Scroll
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(a => {
+        a.classList.remove('active');
+        if (a.getAttribute('href').includes(current)) {
+            a.classList.add('active');
+        }
+    });
+});
